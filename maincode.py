52f4a3490ff62a26abf1e718b3faf8e0d8f7d0be
ethanaholden <160275398+ethanaholden@users.noreply.github.com>
2024-02-16 15:43:29 -0500

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -11,14 +11,6 @@ import pylab
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score  
 import glob
-#import tensorflow as tf
-#from tensorflow.keras.preprocessing.image import ImageDataGenerator
-#from tensorflow.keras.models import Sequential
-#from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
-#from tensorflow.keras.layers import Conv2D, MaxPooling2D
-#from keras import optimizers
-#import keras
-#from tensorflow.keras.utils import to_categorical
 import time
 import IPython.display as ipd
 from audio2numpy import open_audio
@@ -59,11 +51,15 @@ plt.show()
 
 print('delay?')
 
-def audio_delay(path):
+def audio_delay(path, d, atten):
     left = x[0]
-    print(left)
-    left = np.pad(left, (100, 0), mode='constant')
-    left = left[:-100]
+    #print(left)
+    if d != 0:
+        left = np.pad(left, (d, 0), mode='constant')
+        left = left[:-(d)]
+    left = left*(10**(atten/10))
+    print (left)
+    #print (x)
     delayed3 = np.stack([left, x[1]])
     print (delayed3)
     return delayed3
@@ -71,20 +67,36 @@ def audio_delay(path):
 if input() == 'yes':
     x, sr = librosa.load('sound.wav', mono=False)
     xside = np.rot90(x,3)
+    
     outpath = r"C:\Users\joshh\AppData\Local\Programs\Python\Python311\soundout.wav"
     path = r"C:\Users\joshh\AppData\Local\Programs\Python\Python311\sound.wav"
-    delayed_audio10 = audio_delay(path)
-    #print (delayed_audio)
-    print("delayed audio shape",delayed_audio10.shape)
-    print ('original shape',x.shape)
-    #print (xside.shape)
+
+    #delayed_audio0 = audio_delay(path, 0, 0)
+    delayed_audio480 = audio_delay(path, 21, 0)
+    delayed_audio1 = audio_delay(path, 44, 0)
+    delayed_audio10 = audio_delay(path, 441, 0)
+    delayed_audio100 = audio_delay(path, 4410, 0)
+    atten0_15 = audio_delay(path, 0, -1.5)
+    atten0_3 = audio_delay(path, 0, -3)
+    atten0_6 = audio_delay(path, 0, -6)
+    atten21_15 = audio_delay(path, 21, -1.5)
+    atten21_3 = audio_delay(path, 21, -3)
+    atten21_6 = audio_delay(path, 21, -6)
+    
+
     sr = 44100
     testsr = 22050
-    if x.shape[1] == 2:
-        print('hi')
-    #savewav(delayed_audio, outpath, sr)
-    sf.write('hmm.wav', xside, testsr, subtype='PCM_24')
-    sf.write('hmmdel.wav', np.rot90(delayed_audio10,3), testsr, subtype='PCM_24')
-    #savewav(xside, outpath, sr)
-    #write('delayed_audio.wav', sr, x)
+
+    sf.write('teamholden-stereosoundfile-0ms.wav', xside, testsr, subtype='PCM_24')
+    sf.write('teamholden-stereosoundfile-480us.wav', np.rot90(delayed_audio480,3), testsr, subtype='PCM_24')
+    sf.write('teamholden-stereosoundfile-1ms.wav', np.rot90(delayed_audio1,3), testsr, subtype='PCM_24')
+    sf.write('teamholden-stereosoundfile-10ms.wav', np.rot90(delayed_audio10,3), testsr, subtype='PCM_24')
+    sf.write('teamholden-stereosoundfile-100ms.wav', np.rot90(delayed_audio100,3), testsr, subtype='PCM_24')
+
+    sf.write('team[[holden]]-stereosoundfile-[[0ms]]-[[-1.5dB]].wav', np.rot90(atten0_15,3), testsr, subtype='PCM_24')
+    sf.write('team[[holden]]-stereosoundfile-[[0ms]]-[[-3dB]].wav', np.rot90(atten0_3,3), testsr, subtype='PCM_24')
+    sf.write('team[[holden]]-stereosoundfile-[[0ms]]-[[-6dB]].wav', np.rot90(atten0_6,3), testsr, subtype='PCM_24')
+    sf.write('team[[holden]]-stereosoundfile-[[480us]]-[[-1.5dB]].wav', np.rot90(atten21_15,3), testsr, subtype='PCM_24')
+    sf.write('team[[holden]]-stereosoundfile-[[480us]]-[[-3dB]].wav', np.rot90(atten21_3,3), testsr, subtype='PCM_24')
+    sf.write('team[[holden]]-stereosoundfile-[[480us]]-[[-6dB]].wav', np.rot90(atten21_6,3), testsr, subtype='PCM_24')
 print ("done")
